3d0add99d95bb4dfdab1ce6d755371b8b7530ebe
omkark4004 <o.kubal@somaiya.edu>
2022-07-03 23:45:20 +0530

    basic Login & Registration pages

---
+++ register.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+    <link rel="stylesheet" href="registerform.css">
+
+</head>
+<body>
+    <div class="form-container">
+        <form action="" method="post">
+            <h3>Registration Form</h3>
+            <input type="text" name="name" required placeholder="Enter your Name">
+            <br>
+            <br>
+            <input type="text" name="email" required placeholder="Enter your Email">
+            <br>
+            <br>
+            <input type="text" name="password" required placeholder="Enter your Password">
+            <br>
+            <br>
+            <input type="text" name="cpassword" required placeholder="Confirm Password">
+            <br>
+            <br>
+            <select name="user_type" id="user">
+                <option value="user">User</option>
+                <option value="admin">Admin</option>
+            </select>
+            <br>
+            <br>
+            <input type="submit" name="submit" value="Register Now!" class="btn">
+            <p>Already have an account? <a href="login.php">Login </a></p>
+
+
+
+        </form>
+    </div>
+    
+</body>
+</html>
